6ad9283991b29d21736cbec82e2dece632b6b517
Andrey-Kharchenko <gavryk14@gmail.com>
2023-06-15 17:16:19 +0300

    Update 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+  if (intervalId) {
+    return; 
+  }
+
+  startButton.disabled = true; 
+
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false; 
+});
