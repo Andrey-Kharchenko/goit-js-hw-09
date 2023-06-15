428730cd8ba569d720e27a159cd1dde3a1018812
Andrey-Kharchenko <gavryk14@gmail.com>
2023-06-15 22:22:35 +0300

    Update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delayInput = parseInt(form.elements.delay.value);
+  const stepInput = parseInt(form.elements.step.value);
+  const amountInput = parseInt(form.elements.amount.value);
+
+  for (let i = 1; i <= amountInput; i++) {
+    const position = i;
+    const promiseDelay = delayInput + (i - 1) * stepInput;
+
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = { position, delay };
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
